c56bac6ee8a422c45e63ee26e07f3db879ac4710
TruJoe <true.hensley.business@gmail.com>
2026-09-15 11:36:53 -0500

    Read displayed version from Tauri runtime

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { getVersion } from '@tauri-apps/api/app';
 import App from './App';
 import './styles.css';
 import './parity-fixes.css';
+
+if ('__TAURI_INTERNALS__' in window) {
+  void getVersion()
+    .then((version) => document.documentElement.style.setProperty('--torgy-version', `"Torgy ${version}"`))
+    .catch(() => undefined);
+}
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
